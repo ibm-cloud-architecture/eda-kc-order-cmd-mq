3c2efa57a304d5383b6a3c0face768990341424c
vito.sammarco1 <vito.sammarco1@ibm.com>
2021-11-23 16:17:39 +0100

    Unit tests for JMSQueueWriter with configurable payload read from order.json file

---
+++ src/test/ibm/gse/orderms/PriceTest.java
@@ -0,0 +1,44 @@
+package ibm.gse.orderms;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ibm.gse.orderms.domain.model.order.ShippingOrder;
+import ibm.gse.orderms.infra.jms.JMSQueueWriter;
+import org.jboss.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import javax.inject.Inject;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+public class PriceTest {
+
+    private static final Logger log = Logger.getLogger(PriceTest.class);
+
+    @Inject
+    private JMSQueueWriter<ShippingOrder> jmsQueueWriter;
+
+    @Test
+    public void testLastPrice() throws Exception {
+        assertDoesNotThrow(() ->
+            jmsQueueWriter.sendMessage(getOrderFromTestFile(), "DEV.QUEUE.1")
+        );
+    }
+
+    public ShippingOrder getOrderFromTestFile() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        InputStream is = getClass().getClassLoader().getResourceAsStream("order.json");
+        if (is == null)
+            throw new IllegalAccessError("file not found for order json");
+        try {
+            return mapper.readValue(is, mapper.getTypeFactory().constructType(ShippingOrder.class));
+        } catch (Exception e) {
+            log.error("Error reading resource file", e);
+            throw e;
+        }
+    }
+}
